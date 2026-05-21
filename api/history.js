01c576aed14f954c6a1e461a8c486ea3bbaa5c58
Mauricio <mau.alexandrearaujo@gmail.com>
2026-05-21 18:55:54 -0300

    test api

--- api/history.js
+++ api/history.js
@@ -1,26 +1,6 @@
-export default async function handler(req, res) {
-  try {
-    const response = await fetch(
-      "https://www.tipminer.com/br/historico/blaze/double"
-    );
-
-    const html = await response.text();
-
-    const numeros = [...html.matchAll(/\/(\d{1,2})\.png/g)];
-
-    const resultados = numeros.map((m) => {
-      const numero = Number(m[1]);
-
-      return {
-        numero,
-        cor:
-          numero === 0
-            ? "Branco"
-            : numero <= 7
-            ? "Vermelho"
-            : "Preto",
-      };
-    });
-
-    res.status(200).json(resultados.slice(0, 100));
-  } catch (e) {
+export default function handler(req, res) {
+  res.status(200).json({
+    ok: true,
+    mensagem: "API funcionando"
+  });
+}
