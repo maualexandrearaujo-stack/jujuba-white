ec1a9be24f0143195edf3604fd460898a998e934
Mauricio <mau.alexandrearaujo@gmail.com>
2026-05-21 18:50:29 -0300

    fix api route

--- api/history.js
+++ api/history.js
@@ -1,4 +1,5 @@
- try {
+export default async function handler(req, res) {
+  try {
     const response = await fetch(
       "https://www.tipminer.com/br/historico/blaze/double"
     );
@@ -23,8 +24,3 @@
 
     res.status(200).json(resultados.slice(0, 100));
   } catch (e) {
-    res.status(500).json({
-      erro: e.message,
-    });
-  }
-}
