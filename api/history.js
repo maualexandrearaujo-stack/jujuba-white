0d13bcd9dbbea6046500bc23d3d7a27dcd0364e0
Mauricio <mau.alexandrearaujo@gmail.com>
2026-05-21 19:21:58 -0300

    inspect html

--- api/history.js
+++ api/history.js
@@ -6,23 +6,7 @@ export default async function handler(req, res) {
 
     const html = await response.text();
 
-    const numeros = [...html.matchAll(/\/(\d+)\.svg/g)];
-
-    const resultados = numeros.map((m) => {
-      const numero = Number(m[1]);
-
-      return {
-        numero,
-        cor:
-          numero === 0
-            ? "Branco"
-            : numero <= 7
-            ? "Vermelho"
-            : "Preto",
-      };
-    });
-
-    res.status(200).json(resultados.slice(0, 200));
+    res.status(200).send(html.substring(0, 5000));
   } catch (e) {
     res.status(500).json({
       erro: e.message,
