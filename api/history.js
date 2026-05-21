5f5a6cab17344c3523be41d5b10a9d993a62b97c
Mauricio <mau.alexandrearaujo@gmail.com>
2026-05-21 17:33:29 -0300

    regex png parser

--- api/history.js
+++ api/history.js
@@ -1,28 +1,25 @@
-export default async function handler(req, res) {
-  try {
+ try {
     const response = await fetch(
       "https://www.tipminer.com/br/historico/blaze/double"
     );
 
     const html = await response.text();
 
-    const resultados = [];
+    const numeros = [...html.matchAll(/\/(\d{1,2})\.png/g)];
 
-    const regex = /(\d{1,2})\s*(vermelho|preto|branco)/gi;
+    const resultados = numeros.map((m) => {
+      const numero = Number(m[1]);
 
-    let match;
-
-    while ((match = regex.exec(html)) !== null) {
-      resultados.push({
-        numero: Number(match[1]),
+      return {
+        numero,
         cor:
-          match[2].toLowerCase() === "vermelho"
+          numero === 0
+            ? "Branco"
+            : numero <= 7
             ? "Vermelho"
-            : match[2].toLowerCase() === "preto"
-            ? "Preto"
-            : "Branco",
-      });
-    }
+            : "Preto",
+      };
+    });
 
     res.status(200).json(resultados.slice(0, 100));
   } catch (e) {
